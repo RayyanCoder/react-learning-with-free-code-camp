9f9e156d61a596f2f9a001e89d4a637edb38f35e
rayyanlearning <rayyanlearning2022@gmail.com>
2022-10-23 13:28:05 +0530

    First react app
    saw all files of react

---
+++ Day3-30 minutes/jsx practice with components/firstReactProject.js
@@ -0,0 +1,13 @@
+const page =(
+    <div>
+    
+    <ul>
+    <li>was first release in 2013</li>
+    <li>created by jordan walker</li>
+    <li>maintain by face book</li>
+    <li>power thousands of mobile apps</li>
+    </ul>
+    
+    </div>);
+
+    ReactDOM.render(page,document.getElementById("root"));
