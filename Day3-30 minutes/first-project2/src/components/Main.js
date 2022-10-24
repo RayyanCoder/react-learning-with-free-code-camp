f2ab2d3768e532343a144b01a51befc713bae0ad
rayyanlearning <rayyanlearning2022@gmail.com>
2022-10-25 01:59:06 +0530

    Day4-completed
    first project with in local setup

---
+++ Day3-30 minutes/first-project2/src/components/Main.js
@@ -0,0 +1,6 @@
+import React from "react";
+export default function Main(){
+    return(<div>
+        <h1>This is main</h1>
+    </div>);
+}
---
+++ Day3-30 minutes/first-project2/src/components/Navbar.js
@@ -0,0 +1,6 @@
+import React from "react";
+export default function Navbar(){
+    return(<div>
+        <h1>This is nvabar</h1>
+    </div>);
+}
