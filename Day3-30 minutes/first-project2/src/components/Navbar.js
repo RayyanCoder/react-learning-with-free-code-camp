6b17ef1093f94b5db9067fc3bd36ce1cf13bdf63
rayyanlearning <rayyanlearning2022@gmail.com>
2022-10-27 01:30:21 +0530

    Day5-completed
    two projects react infor site,business card

--- Day3-30 minutes/first-project2/src/components/Navbar.js
+++ Day3-30 minutes/first-project2/src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React from "react";
+import './style.css'
 export default function Navbar(){
-    return(<div>
-        <h1>This is nvabar</h1>
-    </div>);
+    return(<nav>
+        <img src="./logo192.png"/>
+        <h3>React Facts</h3>
+        <h4>React Course - Porject 1</h4>
+    </nav>);
 }
