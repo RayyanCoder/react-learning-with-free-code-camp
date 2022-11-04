dbc7fed685e6fb5ea2405d7aebdaceff5a7c1be0
rayyanlearning <rayyanlearning2022@gmail.com>
2022-11-04 20:06:00 +0530

    Counter app project created successfully
    counter app project created successfully

---
+++ counter react app/counter/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Counters from './Counter';
+
+function App() {
+  return (
+    <div >
+      <Counters/>
+    </div>
+  );
+}
+
+export default App;
---
+++ counter react app/counter/src/Counter.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './counter.css';
+
+export default function Counterss(){
+    const [getCount,setCount]=React.useState(1);
+    function decrement(){
+        
+        setCount(count=>count = count+1);
+    }
+    function increment(){
+        
+        setCount(count=>count=count-1);
+    }
+    return(
+        <div>
+       <div className='main-counter'>
+           
+            <div className='increment' for="increment" onClick={decrement}>
+            <h3>+</h3>
+            </div>
+            <div className='count' for="count">
+                <h1>{getCount}</h1>
+            </div>
+            <div className='decrement' for="decrement" onClick={increment}>
+            <h3>-</h3>
+            </div>
+       </div>
+  
+    </div>
+    );
+  
+}
